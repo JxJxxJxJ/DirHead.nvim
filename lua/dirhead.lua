0bd713c01cbcc2efeab67e4bb732f1d7c9319502
JxJxxJxJ <jcruzartico@gmail.com>
2023-02-24 14:22:01 -0300

    make so packer works

--- lua/dirhead.lua
+++ lua/dirhead.lua
@@ -1,14 +1,20 @@
-vim.api.nvim_create_autocmd("BufWritePost", {
-  callback = function(a)
-    if vim.bo[a.buf].buftype ~= "" then return end
+local M = {}
 
-    local line =
-    vim.bo[a.buf].commentstring:gsub("%%s", vim.fn.fnamemodify(a.file, ":~"))
-    local lines = vim.api.nvim_buf_get_lines(a.buf, 0, -1, false)
+function M.setup()
+  vim.api.nvim_create_autocmd("BufWritePost", {
+    callback = function(a)
+      if vim.bo[a.buf].buftype ~= "" then return end
 
-    for i, l in ipairs(lines) do
-      if l == line then return end
-    end
-    vim.api.nvim_buf_set_lines(a.buf, 0, 0, false, { line, "" })
-  end,
-})
+      local line =
+      vim.bo[a.buf].commentstring:gsub("%%s", vim.fn.fnamemodify(a.file, ":~"))
+      local lines = vim.api.nvim_buf_get_lines(a.buf, 0, -1, false)
+
+      for i, l in ipairs(lines) do
+        if l == line then return end
+      end
+      vim.api.nvim_buf_set_lines(a.buf, 0, 0, false, { line, "" })
+    end,
+  })
+end
+
+return M
